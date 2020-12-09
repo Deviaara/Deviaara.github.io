59af4ef28c260799253f34458e71254698f6f7f6
Deviaara <71745257+Deviaara@users.noreply.github.com>
2020-12-10 00:23:51 +0700

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,5 +1,5 @@
 var n = prompt("Ketik nama");
-var c = confirm("HALOOO\n" + n + "\nMAU LIHAT-LIHAT DULU?");
+var c = confirm("HALOOO"\n" + n + "\n"MAU LIHAT-LIHAT DULU?");
 if (c== true){
     alert('BUNGLON TEMAN BAIK');
 } else {
